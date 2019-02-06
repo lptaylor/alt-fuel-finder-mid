9b5280bd90c1eebe83b05300b39c702231206953
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 10:15:13 -0700

    fixes typo

--- app/facades/results_facade.rb
+++ app/facades/results_facade.rb
@@ -1,6 +1,6 @@
 class ResultsFacade
   def self.get_stations(search_location)
     search = AltFuelService.new
-    seach.get_stations(search_location)
+    search.get_stations(search_location)
   end
 end
