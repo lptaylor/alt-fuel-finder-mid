58fe9f3749d0723cc80a3a3102c3ae62f713d89b
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 10:55:39 -0700

    add path helper, fix extra period in div class and fix test to use path helper

--- spec/features/user_can_search_by_zipcode_spec.rb
+++ spec/features/user_can_search_by_zipcode_spec.rb
@@ -9,7 +9,7 @@ describe 'A User' do
     end
   end
   it 'takes me to the path search' do
-    assert_current_path('/search')
+    expect(current_path).to eq(search_path)
   end
   # As a user
   # When I visit "/"
@@ -21,11 +21,11 @@ describe 'A User' do
     assert_selector('.stations', count: 10)
   end
   # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-  xit 'stations should only be Electric and Propane' do
+  it 'stations should only be Electric and Propane' do
 
   end
   # And the stations should be limited to Electric and Propane
-  xit 'each station should have attributes' do
+  it 'each station should have attributes' do
 
   end
   # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
