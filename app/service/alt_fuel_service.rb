7a86412e470da9e5b23f10f2db0d43cebc7e3ba1
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 09:52:41 -0700

    Fills out fuel service

--- app/service/alt_fuel_service.rb
+++ app/service/alt_fuel_service.rb
@@ -1,13 +1,20 @@
 class AltFuelService
 
   def conn
-  Faraday.new(:url => 'https://developer.nrel.gov/api') do |faraday|
-    faraday.headers['x-api-key'] = ENV['NREL_KEY']
-    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    Faraday.new(:url => 'https://developer.nrel.gov/api') do |faraday|
+      faraday.headers['x-api-key'] = ENV['NREL_KEY']
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
   end
 
-  def get_stations
-
-
+  def get_stations(search_location)
+   response = conn.get do |req|
+     req.url '/alt-fuel-stations/v1/nearest.json'
+     req.params['limit'] = 10
+     req.params['radius'] = 6.0
+     req.params['fuel_type'] = 'LPG,ELEC'
+     req.params['location'] = search_location
+   end
+   JSON.parse(response.body)
   end
 end
