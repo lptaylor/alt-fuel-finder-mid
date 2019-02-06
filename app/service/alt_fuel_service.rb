ca0e34659bf9a670d496320218860c2fb31700ec
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 10:14:46 -0700

    fixes url so service works

--- app/service/alt_fuel_service.rb
+++ app/service/alt_fuel_service.rb
@@ -1,20 +1,20 @@
 class AltFuelService
 
   def conn
-    Faraday.new(:url => 'https://developer.nrel.gov/api') do |faraday|
+    Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
       faraday.headers['x-api-key'] = ENV['NREL_KEY']
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      faraday.adapter  Faraday.default_adapter
     end
   end
 
   def get_stations(search_location)
    response = conn.get do |req|
-     req.url '/alt-fuel-stations/v1/nearest.json'
+     req.url '/api/alt-fuel-stations/v1/nearest.json'
      req.params['limit'] = 10
      req.params['radius'] = 6.0
      req.params['fuel_type'] = 'LPG,ELEC'
      req.params['location'] = search_location
    end
-   JSON.parse(response.body)
+   JSON.parse(response.body)['station_counts']
   end
 end
