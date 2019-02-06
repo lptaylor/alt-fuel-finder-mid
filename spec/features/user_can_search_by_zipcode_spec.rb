739d352e10b80d5a8ac6044e1c09559b8a3608d5
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 09:17:06 -0700

    adds tests for full user story as a feature test.

---
+++ spec/features/user_can_search_by_zipcode_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'A User' do
+  it 'takes me to the path search' do
+    visit '/'
+
+    fill_in search_form with: '80203'
+    click_on "Locate"
+
+    expect current_path.to eq('/search')
+  end
+  # As a user
+  # When I visit "/"
+  # And I fill in the search form with 80203 (Note: Use the existing search form)
+  # And I click "Locate"
+  # Then I should be on page "/search"
+
+  xit 'shows list fo 10 stations within 6 miles sorted by distance' do
+    visit '/'
+
+    fill_in search_form with: '80203'
+    click_on "Locate"
+
+    assert_selector('.stations', count: 10)
+  end
+  # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+  xit 'stations should only be Electric and Propane' do
+
+  end
+  # And the stations should be limited to Electric and Propane
+  xit 'each station should have attributes' do
+    
+  end
+  # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+end
